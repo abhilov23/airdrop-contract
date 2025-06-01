1d9704aa91a4fa36aaef43367ac77c462e36c6da
Abhilov23 <abhilovgupta01@gmail.com>
2025-06-01 22:15:43 +0530

    implemented and tested signature verification

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -22,6 +22,7 @@ contract MerkleAirdropTest is Test {
         bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
         bytes32[] public PROOF = [proofOne, proofTwo];
         uint256 public AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
+        address gasPayer;
         address user;
         uint256 userPrivKey;
 
@@ -36,14 +37,21 @@ contract MerkleAirdropTest is Test {
           //  token.mint(token.owner(), AMOUNT_TO_SEND);
           //  token.transfer(address(airdrop), AMOUNT_TO_SEND);
            (user, userPrivKey) = makeAddrAndKey("user"); 
+           gasPayer = makeAddr("gasPayer");
         }
 
 
     function testUsersCanClaim() public {
       uint256 startingBalance = token.balanceOf(user);
+      bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
 
-      vm.prank(user);
-      airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+      //sign a message
+      (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivKey, digest);
+
+
+      //gaspayer called the claim using the signed message
+      vm.prank(gasPayer);
+      airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
 
       uint256 endingBalance = token.balanceOf(user);
       console.log("ending balance", endingBalance);
